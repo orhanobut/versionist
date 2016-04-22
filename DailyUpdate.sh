59ad1567349e124ca5f2bbb69e51afcd33e8a9d2
orhanobut <orhanobut@gmail.com>
2016-04-22 09:46:29 +0200

    Add daily update script

---
+++ DailyUpdate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+NOW=$(date +%F)
+BRANCH_NAME="update-$NOW"
+git checkout master
+git pull origin master
+
+echo "$BRANCH_NAME branch is created"
+git checkout -b $BRANCH_NAME
+
+echo "ReadMeGenerator is running"
+python ReadMeGenerator.py
+
+echo "Committing new changes"
+git add .
+git commit -am "Update versions"
+
+echo "Pushing the new branch"
+git push origin $BRANCH_NAME
